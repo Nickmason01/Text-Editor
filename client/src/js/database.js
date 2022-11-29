1a6f4e3fd4c76891cdef69b31225533b9d8f3da2
Nick Mason <nickmason372@yahoo.com>
2022-11-29 18:50:37 -0500

    anifest-json workarounds

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  const newTransaction = jateDb.transaction('jate', 'readwrite');
 
+  const store = newTransaction.objectStore('jate');
+
+  const request = store.add({id: 1, value: content});
+
+  const result = await request;
+
+  console.log("data saved!", result);
+  
+}
+  
+  
+    // TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const transaction = jateDb.transaction('jate', 'readonly');
+  const store = transaction.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  return result; 
+ 
+}
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,58 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Editor'
+      }),
+      new InjectManifest({
+        swSrc:'./src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'PWA-Text-Editor',
+        short_name: 'Edit',
+        description: 'Simple Text-Editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        crossorigin:'use-credentials',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+            purpose: 'maskable'
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [ 'style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'assets/resources'
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
